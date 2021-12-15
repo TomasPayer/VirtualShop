e02d13f42054d8e5512be01474476a64b8143c96
tomas.payer <tomaspayer18@gmail.com>
2021-12-15 15:56:40 -0300

    cambios

--- products.js
+++ products.js
@@ -2,6 +2,6 @@ const productos = [
     { id: 1, nombre: "Bose", foto: "https://eloutput.com/app/uploads-eloutput.com/2021/01/Bose-QC-Earbuds-estuche-carga.jpg", precio: 1500 },
     { id: 2, nombre: "Macbook", foto: "https://eloutput.com/app/uploads-eloutput.com/2021/01/Bose-QC-Earbuds-estuche-carga.jpg", precio: 1500 },
     { id: 3, nombre: "Iphone", foto: "https://eloutput.com/app/uploads-eloutput.com/2021/01/Bose-QC-Earbuds-estuche-carga.jpg", precio: 1500 },
-    { id: 4, nombre: "Samsung", foto: "./img/patamuslo.jpg", precio: 1500 }
+    { id: 4, nombre: "Smart Watch", foto: "./img/patamuslo.jpg", precio: 1500 }
 
 ];
